f195cadcc49add9295823eae1bac75316b925006
= <evebramley@me.com>
2019-09-28 11:17:43 +0100

    set state for new values

---
+++ src/components/NewArticleForm.js
@@ -0,0 +1,36 @@
+import React { Component } from 'react';
+
+class NewArticleForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      content: '',
+      summary: '',
+      journalist: '',
+      category: ['']
+    };
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleSummaryChange = this.handleSummaryChange.bind(this);
+    this.handleJournalistChange = this.handleJournalistChange.bind(this);
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+  }
+
+  handleTitleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+  handleContentChange(event) {
+    this.setState({ content: event.target.value });
+  }
+  handleSummaryChange(event) {
+    this.setState({ summary: event.target.value });
+  }
+  handleJournalistChange(event) {
+    this.setState({ journalist: event.target.value });
+  }
+  handleCategoryChange(event) {
+    this.setState({ category: event.target.value });
+  }
+}
