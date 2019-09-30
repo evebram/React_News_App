3c86d38c3d58c68149298b1f9dc3853baddcd88a
= <evebramley@me.com>
2019-09-30 12:03:18 +0100

    End of: form draft one

--- src/components/NewArticleForm.js
+++ src/components/NewArticleForm.js
@@ -13,14 +13,16 @@ class NewArticleForm extends Component {
       journalist: '',
       date: '',
       summary: '',
+      image: '',
       content: '',
-      category: ['']
+      category: []
     };
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handleJournalistChange = this.handleJournalistChange.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleSummaryChange = this.handleSummaryChange.bind(this);
+    this.handleImageChange = this.handleImageChange.bind(this);
     this.handleContentChange = this.handleContentChange.bind(this);
     this.handleCategoryChange = this.handleCategoryChange.bind(this);
   }
@@ -31,11 +33,19 @@ class NewArticleForm extends Component {
   handleJournalistChange(event) {
     this.setState({ journalist: event.target.value });
   }
-  handleDateChange = date => {
-  const [startDate, setStartDate] = useState(new Date());
-};
+//   handleDateChange() = date => {
+//   const [startDate, setStartDate] = useState(new Date());
+// };
+
+  handleDateChange(event) {
+    this.setState({ date: event.target.value });
+  }
+
   handleSummaryChange(event) {
     this.setState({ summary: event.target.value });
+  }
+  handleImageChange(event) {
+    this.setState({ image: event.target.value });
   }
   handleContentChange(event) {
     this.setState({ content: event.target.value });
@@ -52,6 +62,7 @@ class NewArticleForm extends Component {
       journalist: this.state.journalist,
       date: this.state.date,
       summary: this.state.summary,
+      image: this.state.image,
       content: this.state.content,
       category: this.state.category
     };
@@ -61,9 +72,9 @@ class NewArticleForm extends Component {
       journalist: '',
       date: '',
       summary: '',
+      image: '',
       content: '',
       category: []
-      // category: ['']
     });
   }
 
@@ -106,8 +117,8 @@ class NewArticleForm extends Component {
       </FormGroup>
 
       <FormGroup>
-        <label htmlFor="summary">Summary: </label>
-        <input
+        <Label htmlFor="summary">Summary: </Label>
+        <Input
           id="summary"
           type="text"
           value={this.state.summary}
@@ -121,8 +132,8 @@ class NewArticleForm extends Component {
       </FormGroup>
 
       <FormGroup>
-        <label htmlFor="content">Story: </label>
-        <input
+        <Label htmlFor="content">Story: </Label>
+        <Input
           id="content"
           type="text"
           value={this.state.content}
@@ -130,22 +141,24 @@ class NewArticleForm extends Component {
         />
       </FormGroup>
 
-      <FormGroup>
-      <label htmlFor="category">Category: </label>
+      <FormGroup tag="fieldset">
+      <Label htmlFor="category">Category: </Label>
+        <FormGroup check>
           {categories.map((category, index) =>
-              <label key={index}>
+              <Label check key={index}>
                 {category}
-                <input
+                <Input
                 value={category}
                 checked={this.state.taggedCategories === category.toUpperCase()}
                 onChange={this.handleCategoryChange}
-                type="radio" />
-              </label>
+                type="radio" />{''}
+              </Label>
             )}
+        </FormGroup>
       </FormGroup>
 
       <FormGroup>
-        <input type="submit" />
+        <Input type="submit" />
       </FormGroup>
 
       </Form>
