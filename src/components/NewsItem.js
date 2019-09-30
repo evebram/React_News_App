e8b810909c31f5056f7baf8c897c98ab2e8277d7
Graeme Henderson <graeme.189@gmail.com>
2019-09-30 14:20:10 +0100

    Add rating counter

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './NewsItem.css';
 import Collapsible from 'react-collapsible';
 const NewsItem = (props) => {
+
+  const handleClick = () => {
+    props.article.rating += 1;
+    console.log(props.article.rating);
+  }
+
  return (
    <>
      <hr />
@@ -11,7 +17,7 @@ const NewsItem = (props) => {
      </div>
      <h2>{props.article.title}</h2>
      <h4>{props.article.summary}</h4>
-     <Collapsible trigger="Full story"><p>{props.article.content}</p></Collapsible>
+     <button onClick={handleClick}><Collapsible trigger="Full story" ><p>{props.article.content}</p></Collapsible></button>
    </>
  )
 }
