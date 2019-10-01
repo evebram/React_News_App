a0d7aecfceddd87d031dd52795378cfb60ac2698
bchutchison <benhutchison85@gmail.com>
2019-10-01 09:13:12 +0100

    Content drop down added to top story

--- src/components/TopNewsItem.js
+++ src/components/TopNewsItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './TopNewsItem.css';
+import Collapsible from 'react-collapsible';
 
 const TopNewsItem = (props) => {
+
+  const handleClick = () => {
+    props.topArticle.rating += 1;
+    console.log(props.topArticle.rating);
+  }
 
 
   return (
     <div className='top-story'>
       <h3>{props.topArticle.title}</h3>
       <h5>{props.topArticle.summary}</h5>
+       <button onClick={handleClick}><Collapsible trigger="Full story" ><p>{props.topArticle.content}</p></Collapsible></button>
     </div>
   )
 }
